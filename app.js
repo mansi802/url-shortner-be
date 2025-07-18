e269ea172e992e66123cea6c01253a5aefb28768
Mansi <mansisingla802@gmail.com>
2025-07-19 00:49:57 +0530

    fix: adjust CORS configuration for development and production environments

--- app.js
+++ app.js
@@ -13,8 +13,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, // your React app
-    credentials: true, // 👈 this allows cookies to be sent
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.FRONTEND_URL
+        : "http://localhost:5173",
+    credentials: true,
   })
 );
 
